ed57283fda10be07e64bdad2f856713e70a3ccd8
Istratov Dmitrii <funkill2@gmail.com>
2020-06-08 17:22:09 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,10 @@ set -x
 
 cd ${GITHUB_WORKSPACE}
 
+echo "RunID: $GITHUB_RUN_ID"
+echo "Sha: $GITHUB_SHA"
+echo "REf: $GITHUB_REF"
+echo "Head ref: $GITHUB_HEAD_REF"
+echo "Base ref: $GITHUB_BASE_REF"
+
 RUST_BACKTRACE=full bugs-checker -o second-edition/src -t second-edition-ru/src
